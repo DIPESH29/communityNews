7e85de690dfb98e3d9bda3d44ff6e44718e3e93e
DIPESH29 <dipeshmandloi9@gmail.com>
2021-09-03 20:37:35 +0530

    uploading community News for first time

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>©  2021 News | Powered by <a href="https://github.com/DIPESH29/">DIPESH MANDLOI</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
